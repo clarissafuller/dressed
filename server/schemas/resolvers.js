081612227abb9bc3e434dcf00bc0e44764fc55f0
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2024-01-04 12:52:18 -0500

    fixes file structure to get page showing up from client side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,45 +1,27 @@
-const { Profile } = require('../models');
+const { Tech, Matchup } = require('../models');
 
 const resolvers = {
-  // Important for useQuery: The resolver matches the typeDefs entry point and informs the request of the relevant data
   Query: {
-    profiles: async () => {
-      return Profile.find();
+    tech: async () => {
+      return Tech.find({});
     },
-
-    // Important for Query Variables: Each query resolver function can accept up to four parameters.
-    // The second parameter, commonly referred to as "args," represents the variable argument values passed with the query.
-    // It is always an object, and in this case, we are destructuring that object to retrieve the profileId value.
-    profile: async (parent, { profileId }) => {
-      return Profile.findOne({ _id: profileId });
+    matchups: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Matchup.find(params);
     },
   },
-  // Important for useMutation: The resolver matches the typeDefs entry point and informs the request of the relevant data
   Mutation: {
-    addProfile: async (parent, { name }) => {
-      return Profile.create({ name });
+    createMatchup: async (parent, args) => {
+      const matchup = await Matchup.create(args);
+      return matchup;
     },
-    addSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        {
-          $addToSet: { skills: skill },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    },
-    removeProfile: async (parent, { profileId }) => {
-      return Profile.findOneAndDelete({ _id: profileId });
-    },
-    removeSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        { $pull: { skills: skill } },
+    createVote: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
         { new: true }
       );
+      return vote;
     },
   },
 };
