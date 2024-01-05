122b3cdc4916891d8415bb1d474d7735187d00a3
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2024-01-05 12:05:28 -0500

    adds all buttons and icons to the header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
 import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faMagnifyingGlass,
+  faCircleUser,
+  faShirt,
+  faBagShopping,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <button type="button" className="menu-button">
+          <FontAwesomeIcon className="icon" icon={faBars} />
+        </button>
         <div className="search-container">
           <input type="text" placeholder="Search..." className="search-input" />
           <button type="button" className="search-button">
-            Search
+            <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />
           </button>
         </div>
         <div className="text-container">
@@ -17,6 +28,15 @@ const Header = () => {
             <p className="m-0">effortless style without commitment</p>
           </Link>
         </div>
+        <button type="button" className="profile-button">
+          <FontAwesomeIcon className="icon" icon={faCircleUser} />
+        </button>
+        <button type="button" className="closet-button">
+          <FontAwesomeIcon className="icon" icon={faShirt} />
+        </button>
+        <button type="button" className="order-button">
+          <FontAwesomeIcon className="icon" icon={faBagShopping} />
+        </button>
       </div>
     </header>
   );
